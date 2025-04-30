08d712bef07b8e8d31de2b2897ff8e1ebde241f7
Anton Lu <anton.lu@cern.ch>
2025-04-30 15:38:47 +0200

    add GPyConstrainedParameter

--- sa_preisach/nn/__init__.py
+++ sa_preisach/nn/__init__.py
@@ -1,8 +1,9 @@
 from .._mod_replace import replace_modname
 from ._binary_parameter import BinaryParameter
 from ._constrained_parameter import ConstrainedParameter
+from ._gpy_constrained_parameter import GPyConstrainedParameter
 
-for _mod in (BinaryParameter, ConstrainedParameter):
+for _mod in (BinaryParameter, ConstrainedParameter, GPyConstrainedParameter):
     replace_modname(_mod, __name__)
 
 del _mod
@@ -11,4 +12,5 @@ del replace_modname
 __all__ = [
     "BinaryParameter",
     "ConstrainedParameter",
+    "GPyConstrainedParameter",
 ]
---
+++ sa_preisach/nn/_gpy_constrained_parameter.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import typing
+
+import gpytorch.constraints
+import torch
+
+
+class GPyConstrainedParameter(torch.nn.Module):
+    def __init__(
+        self,
+        data: torch.Tensor,
+        constraint: gpytorch.constraints.Interval,
+        *,
+        requires_grad: bool = True,
+        **kwargs: typing.Any,
+    ):
+        super().__init__()
+        self.constraint = constraint
+
+        self.raw_parameter = torch.nn.Parameter(
+            self.inverse(data), requires_grad=requires_grad, **kwargs
+        )
+
+    def forward(self) -> torch.Tensor:
+        return self.constraint.transform(self.raw_parameter)
+
+    def inverse(self, x: torch.Tensor) -> torch.Tensor:
+        return self.constraint.inverse_transform(x)
+
+    @property
+    def data(self) -> torch.Tensor:
+        return self.raw_parameter
+
+    @property
+    def value(self) -> torch.Tensor:
+        return self.forward()
